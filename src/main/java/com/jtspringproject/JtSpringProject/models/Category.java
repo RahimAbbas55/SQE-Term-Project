7f2051d3e9288e4d09b9e5cd8d39c2e45dff2ab8
maham-q <maham.qadeer2003@gmail.com>
2023-12-01 14:56:28 +0500

    Committing changes to src/.DS_Store and adding .idea/

---
+++ src/main/java/com/jtspringproject/JtSpringProject/models/Category.java
@@ -0,0 +1,34 @@
+package com.jtspringproject.JtSpringProject.models;
+
+import javax.persistence.*;
+
+@Entity(name="CATEGORY")
+public class Category {
+	private int category_counter = 0;
+	@Id
+	@Column(name = "category_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+
+	public Category(){
+		this.id = category_counter++;
+	}
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
